c424ffbfe11bd1e3ab61bebc27c97fbde86258b5
SteveImmanuel <steveandreasimanuel@gmail.com>
2019-04-21 22:44:16 +0700

    finishing everything

--- js/effect.js
+++ js/effect.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
     function resizePanel(){
         outHeight=document.getElementById("avatar-screen").offsetHeight;
         document.getElementById("main-screen").style.minHeight=outHeight*0.7+"px";
-        // alert("outHeight="+outHeight);
     }
     
     $("#sendButton").click(
@@ -28,42 +27,28 @@ $(document).ready(function(){
             $("#chat-text").fadeOut(1000);
             $("#answer-text").fadeOut(1000);
             var textInput=document.getElementById("chat-box").value;
-            move(90);
-            function move(max) {
-                var elem = document.getElementById("lvl-conf");   
-                var elemParent=document.getElementById("bar-conf");
-                var width = (parseFloat(getComputedStyle(elem).width)/parseFloat(getComputedStyle(elemParent).width))*100;
-                width=Math.round(width);
-                var id = setInterval(frame, 10);
-                function frame() {
-                    if (width == max) {
-                        clearInterval(id);
-                        window.location.href = "chatbot.php?chat=" + textInput+"&percentage="+max;
-                    } else if(width<max) {
-                        width++; 
-                        elem.style.width = width + '%'; 
-                        elem.innerHTML = width  + '%';
-                    } else if(width>max){
-                        width--; 
-                        elem.style.width = width + '%'; 
-                        elem.innerHTML = width  + '%';   
-                    }
-                }
-            }
+            window.location.href = "chatbot.php?chat="+textInput;
         }
         );
 });
 
-function Test(){
-    var msg = new SpeechSynthesisUtterance('Halo nama saya dicky');
-    var voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[10];
-    msg.voiceURI = "native";
-    msg.volume = 1;
-    msg.rate = 1;
-    msg.pitch = 0.8;
-    msg.text = "awdad";
-    msg.lang = '-US';
-    speechSynthesis.speak(msg);
+function move(max) {
+    var elem = document.getElementById("lvl-conf");   
+    var elemParent=document.getElementById("bar-conf");
+    var width = (parseFloat(getComputedStyle(elem).width)/parseFloat(getComputedStyle(elemParent).width))*100;
+    width=Math.round(width);
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width == max) {
+            clearInterval(id);
+        } else if(width<max) {
+            width++; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width  + '%';
+        } else if(width>max){
+            width--; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width  + '%';   
+        }
+    }
 }
-        
